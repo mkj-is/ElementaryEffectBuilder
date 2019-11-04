cc4cefacdb083230bda3de8cfd62766911fd6fbd
Matěj Kašpar Jirásek <matej.jirasek@me.com>
2019-11-04 09:14:02 +0100

    Rename to elementary effect builder

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "CombineSaga",
+    name: "ElementaryEffectBuilder",
     platforms: [
         .iOS(.v13),
         .macOS(.v10_15),
@@ -14,8 +14,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "CombineSaga",
-            targets: ["CombineSaga"]),
+            name: "ElementaryEffectBuilder",
+            targets: ["ElementaryEffectBuilder"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -25,10 +25,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "CombineSaga",
+            name: "ElementaryEffectBuilder",
             dependencies: []),
         .testTarget(
-            name: "CombineSagaTests",
-            dependencies: ["CombineSaga"]),
+            name: "ElementaryEffectBuilderTests",
+            dependencies: ["ElementaryEffectBuilder"]),
     ]
 )
