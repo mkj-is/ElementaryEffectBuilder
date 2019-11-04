eac593ec34ba4655e4bc765e4fcd14c4e81c9ca6
Matěj Kašpar Jirásek <matej.jirasek@me.com>
2019-11-04 11:33:34 +0100

    Add dependency on Elementary

--- Package.swift
+++ Package.swift
@@ -12,19 +12,17 @@ let package = Package(
         .tvOS(.v13)
     ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "ElementaryEffectBuilder",
             targets: ["ElementaryEffectBuilder"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(path: "../Elementary"),
     ],
     targets: [
         .target(
             name: "ElementaryEffectBuilder",
-            dependencies: []),
+            dependencies: ["Elementary"]),
         .testTarget(
             name: "ElementaryEffectBuilderTests",
             dependencies: ["ElementaryEffectBuilder"]),
--- Sources/ElementaryEffectBuilder/Functions/combine.swift
+++
@@ -1,9 +0,0 @@
-public func combine<T>(_ functions: [(T) -> Void]) -> (T) -> Void {
-    { parameter in
-        functions.forEach { $0(parameter) }
-    }
-}
-
-public func combine<T>(_ functions: (T) -> Void...) -> (T) -> Void {
-    combine(functions)
-}
